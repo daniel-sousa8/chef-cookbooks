96ebb0256e5b5feb645e9ca6db0ba1447aa85756
daniel_sousa_ <daniel.sousa@rivendel.com.br>
2016-08-20 22:06:09 -0300

    adicionando receita do zabbix-agent

---
+++ zabbix-agent/recipes/default.rb
@@ -0,0 +1,61 @@
+#
+# Cookbook Name:: zabbix-agent
+# Recipe:: default
+#
+# Copyright 2015, SCUP
+#
+
+case node[:platform]
+    when 'redhat', 'centos', 'amazon'
+
+execute 'zabbix_install_r' do
+  Chef::Log.info("Downloading and installing zabbix")
+  user 'root'
+  command 'yum localinstall http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm -y && yum install zabbix-agent -y'
+  action :run
+end
+
+   when 'ubuntu'
+
+execute 'zabbix_install_u' do
+  Chef::Log.info("Downloading and installing zabbix")
+  user 'root'
+  command 'cd /tmp && wget http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.4-1+trusty_all.deb && dpkg -i zabbix-release_2.4-1+trusty_all.deb && apt-get update && apt-get install zabbix-agent && rm zabbix-release_2.4-1+trusty_all.deb'
+  action :run
+end
+
+   when 'debian'
+
+  execute 'zabbix_install_d' do
+  Chef::Log.info("Downloading and installing zabbix")
+  user 'root'
+  command 'cd /tmp && wget http://repo.zabbix.com/zabbix/2.4/debian/pool/main/z/zabbix-release/zabbix-release_2.4-1+wheezy_all.deb && dpkg -i zabbix-release_2.4-1+wheezy_all.deb && apt-get update && apt-get install zabbix-agent && rm zabbix-release_2.4-1+wheezy_all.deb'
+  action :run
+end
+
+end
+
+template '/etc/zabbix/zabbix_agentd.conf' do
+  source 'zabbix_agentd.erb'
+  owner 'zabbix'
+  group 'zabbix'
+  mode '0644'
+end
+
+directory '/etc/zabbix/scripts' do
+ owner 'root'
+ group 'root'
+ mode '0755'
+ action :create
+end
+
+template '/etc/zabbix/scripts/check-service.sh' do
+  source 'check-service.sh'
+  owner 'zabbix'
+  group 'zabbix'
+  mode '0755'
+end
+
+service "zabbix-agent" do
+  action [ :enable, :restart ]
+end
